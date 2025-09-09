aae8a4dc1210f0bbc84b2b38dc9fcc22ea35885e
kbaddepuri <kbaddepuri@salesforce.com>
2025-09-09 19:10:55 +0530

    Add video call functionality to index.html and update server.js for WebRTC signaling

--- server.js
+++ server.js
@@ -1,31 +1,24 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
 io.on("connection", (socket) => {
   console.log("User connected");
 
   socket.on("chat message", (data) => {
-    // data = { user, text }
-    // Broadcast to everyone except sender
     socket.broadcast.emit("chat message", data);
+  });
+
+  // WebRTC signaling
+  socket.on("offer", (offer) => {
+    socket.broadcast.emit("offer", offer);
+  });
+
+  socket.on("answer", (answer) => {
+    socket.broadcast.emit("answer", answer);
+  });
+
+  socket.on("ice-candidate", (candidate) => {
+    socket.broadcast.emit("ice-candidate", candidate);
   });
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
