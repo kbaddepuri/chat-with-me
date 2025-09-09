20d8dfb285530fde1be26718950f67cff9759766
kbaddepuri <kbaddepuri@salesforce.com>
2025-09-09 17:41:45 +0530

    Updated video call with highfy look

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js (Node + Express + Socket.IO)
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -5,53 +6,24 @@ const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+const io = new Server(server, {
+  cors: { origin: "*" }
 });
 
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.use("/static", express.static(path.join(__dirname, "static"))); // optional
+
 io.on("connection", (socket) => {
-  console.log("User connected");
+  console.log("User connected:", socket.id);
 
-  socket.on("chat message", (data) => {
-    // data = { user, text }
-    // Broadcast to everyone except sender
-    socket.broadcast.emit("chat message", data);
-  });
+  socket.on("chat message", (data) => socket.broadcast.emit("chat message", data));
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
+  socket.on("offer", (offer) => socket.broadcast.emit("offer", offer));
+  socket.on("answer", (answer) => socket.broadcast.emit("answer", answer));
+  socket.on("ice-candidate", (candidate) => socket.broadcast.emit("ice-candidate", candidate));
+
+  socket.on("disconnect", () => console.log("User disconnected:", socket.id));
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-io.on("connection", (socket) => {
-  console.log("User connected");
-
-  socket.on("chat message", (data) => {
-    socket.broadcast.emit("chat message", data);
-  });
-
-  // WebRTC signaling
-  socket.on("offer", (offer) => {
-    socket.broadcast.emit("offer", offer);
-  });
-
-  socket.on("answer", (answer) => {
-    socket.broadcast.emit("answer", answer);
-  });
-
-  socket.on("ice-candidate", (candidate) => {
-    socket.broadcast.emit("ice-candidate", candidate);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-});
-
+server.listen(PORT, () => console.log(`Server on ${PORT}`));
