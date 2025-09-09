f37e8587699699f7b75eaa3e1e0fa1131d469e82
kbaddepuri <kbaddepuri@salesforce.com>
2025-09-09 17:34:18 +0530

    video call support

--- server.js
+++ server.js
@@ -30,3 +30,28 @@ server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("chat message", (data) => {
+    socket.broadcast.emit("chat message", data);
+  });
+
+  // WebRTC signaling
+  socket.on("offer", (offer) => {
+    socket.broadcast.emit("offer", offer);
+  });
+
+  socket.on("answer", (answer) => {
+    socket.broadcast.emit("answer", answer);
+  });
+
+  socket.on("ice-candidate", (candidate) => {
+    socket.broadcast.emit("ice-candidate", candidate);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
